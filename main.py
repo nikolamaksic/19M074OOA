13cc57484eb8f1981f0c78881f1d3e4a7e3e3ebb
Dzoni <nikola7amg@gmail.com>
2021-08-31 00:11:22 +0200

    Changed crossover, now it takes first few cubes from one parent and move cubes from second parent in space until they doesn't overlap with other cubes.

--- main.py
+++ main.py
@@ -5,12 +5,13 @@ import plotly.graph_objects as go
 import numpy as np
 import math as ma
 import time
+import copy
 
 # parameters
 axis_size = 100
-popul_size = 10
+popul_size = 20
 num_of_alg_iter = 50
-prob_to_mutate = 0.5
+prob_to_mutate = 0.05
 cub_list = [x for x in range(1,21)]
 im_folder = r'.\img\\'
 
@@ -29,15 +30,17 @@ def find_space_bounds(cubes):
         z_max_a.append(v[2]+k)
     x_min, y_min, z_min = min(x_min_a),min(y_min_a),min(z_min_a)
     x_max, y_max, z_max = max(x_max_a),max(y_max_a),max(z_max_a)
-    x_size = x_max-x_min
-    y_size = y_max-y_min
-    z_size = z_max-z_min
+
     
-    return x_min, y_min, z_min, x_size, y_size,z_size
+    return x_min, y_min, z_min, x_max, y_max, z_max
 
 def draw_and_save_space(cubes,it):  
     # Create axis
-    x_min, y_min, z_min, x_size, y_size,z_size = find_space_bounds(cubes)
+    x_min, y_min, z_min, x_max, y_max, z_max = find_space_bounds(cubes)
+    
+    x_size = x_max-x_min
+    y_size = y_max-y_min
+    z_size = z_max-z_min
     ax_s = max([x_size, y_size,z_size])
     # axes = [x_size, y_size, z_size]
     axes = [ax_s, ax_s, ax_s]
@@ -73,7 +76,13 @@ def draw_and_save_space(cubes,it):
     plt.ioff()
     plt.savefig(im_folder+'\\'+str(it)+'.PNG',bbox_inches='tight')   
 
-def check_overlap(cubes,k):
+def check_two_cubes_overlap(cubeA,cubeA_size,cubeB,cubeB_size):
+    ret = cubeA[0]+cubeA_size > cubeB[0] and cubeA[0] < cubeB[0]+cubeB_size and\
+               cubeA[1]+cubeA_size > cubeB[1] and cubeA[1] < cubeB[1]+cubeB_size and\
+               cubeA[2]+cubeA_size > cubeB[2] and cubeA[2] < cubeB[2]+cubeB_size
+    return ret
+
+def current_cube_position_overlap(cubes,k):
     """
     Check if the given cubes is overlapping other cubes in array.
 
@@ -95,15 +104,21 @@ def check_overlap(cubes,k):
     comp_c = cubes[k]
     for cub_s,c in cubes.items():
         if(cub_s!=k):
-            if(cubes[k][0]+k > c[0] and cubes[k][0] < c[0]+cub_s and\
-               cubes[k][1]+k > c[1] and cubes[k][1] < c[1]+cub_s and\
-               cubes[k][2]+k > c[2] and cubes[k][2] < c[2]+cub_s):
+            # if(cubes[k][0]+k > c[0] and cubes[k][0] < c[0]+cub_s and\
+            #    cubes[k][1]+k > c[1] and cubes[k][1] < c[1]+cub_s and\
+            #    cubes[k][2]+k > c[2] and cubes[k][2] < c[2]+cub_s):
+            if(check_two_cubes_overlap(cubes[k],k,c,cub_s)):
                 return True
 
     return False
     
-    
-    
+def check_two_array_cubes_overlaping(c1, c2):
+    for k1, v1 in c1.items():
+        for k2, v2 in c2.items():
+            if(check_two_cubes_overlap(v1,k1,v2,k2)):
+                return True
+    return False
+
 def create_population(pop_size):
     """
     Create initial population with given size
@@ -154,6 +169,7 @@ def calculate_fitness_for_individual(cubes):
         z_max_a.append(v[2]+k)
     x_min, y_min, z_min = min(x_min_a),min(y_min_a),min(z_min_a)
     x_max, y_max, z_max = max(x_max_a),max(y_max_a),max(z_max_a)
+    # V = (x_max-x_min)*(y_max-y_min)*(z_max-z_min)
     V = (x_max-x_min)*(y_max-y_min)*(z_max-z_min)
     return  1/V # biger fitness = better individua
 
@@ -187,38 +203,66 @@ def find_best_individual(p):
     V = int(round(1/max_fit)) # Volume
     return indiv, ind, V
 
+def append_second_parent_features_to_child(current_ch, sec_par_cub):
+    new_ch = current_ch
+    x_min_ch, y_min_ch, z_min_ch, x_max_ch, y_max_ch, z_max_ch = find_space_bounds(current_ch)
+    x_min_par, y_min_par, z_min_par, x_max_par, y_max_par, z_max_par = find_space_bounds(sec_par_cub)
+    for k in sec_par_cub:
+        sec_par_cub[k][0] = sec_par_cub[k][0] + x_min_ch - x_min_par
+        sec_par_cub[k][1] = sec_par_cub[k][1] + y_min_ch - y_min_par
+        sec_par_cub[k][2] = sec_par_cub[k][2] + z_min_ch - z_min_par
+    pom_sec_par = copy.deepcopy(sec_par_cub)
+    if(not check_two_array_cubes_overlaping(sec_par_cub, current_ch)):
+        for k, v in pom_sec_par.items():
+            new_ch[k] = v
+        return new_ch
+    for _ in range(x_min_ch, x_max_ch + 1):
+        for k in pom_sec_par:
+            pom_sec_par[k][0] += 1
+            pom_sec_par[k][1] = sec_par_cub[k][1]
+        
+        for _ in range(y_min_ch, y_max_ch + 1):
+            
+            for k in pom_sec_par:
+                pom_sec_par[k][1] += 1
+                pom_sec_par[k][2] = sec_par_cub[k][2]
+            
+            for _ in range(y_min_ch, y_max_ch + 1):
+                for k in pom_sec_par:
+                    pom_sec_par[k][2] += 1
+                if(not check_two_array_cubes_overlaping(pom_sec_par, current_ch)):
+                    for k, v in pom_sec_par.items():
+                        new_ch[k] = v
+                    return new_ch
+                      
+    assert False     
+                    
+                    
+    
+
 def crossover(par1, par2):
-    d = 5
+    d = 4
     cross_pos = int(round(len(cub_list)/d))
     ch1 = {}
     ch2 = {}
     ind = 0
+    p1 = {}
+    p2 = {}
     for k in par1:
         if(ind<=cross_pos):
             ch1[k] = par1[k]
-            ch2[k] = par2[k]
+            p1[k]  = par2[k]
         else:
             ch2[k] = par1[k]
-            ch1[k] = par2[k]
+            p2[k]  = par2[k]
         ind+=1
-    ch1 = fix_child(ch1)
-    ch2 = fix_child(ch2)
+    
+    ch1 = append_second_parent_features_to_child(ch1,p2)
+    ch2 = append_second_parent_features_to_child(ch2,p1)
+
     return ch1, ch2
 
-def fix_child(cub):    
-    num_of_cubes = len(cub_list)
-    k = 0
-    while k<num_of_cubes:
-        if(check_overlap(cub,cub_list[k])): 
-            direct = np.random.randint(0,3)
-            cub[cub_list[k]][direct] =   np.random.randint(axis_size//2)
-            # cub[cub_list[k]] = np.random.randint(100, size=3)
-            # check if we found place where new cube is not overlaping other ones
-            if(not check_overlap(cub,cub_list[k])): 
-                k += 1
-        else:
-            k+=1
-    return  cub
+
 
 
 def mutate(cub):
@@ -227,7 +271,7 @@ def mutate(cub):
     while(not finished):
         cub[cub_to_mut] = np.random.randint(axis_size//2, size=3) # xmin, ymin, zmin
         # check if we found place where new cube is not overlaping other ones
-        if(not check_overlap(cub,cub_to_mut)): 
+        if(not current_cube_position_overlap(cub,cub_to_mut)): 
             finished = True
     return cub
     
@@ -259,7 +303,7 @@ def generate_next_population(popul):
         fit_sum += norm_fit[ind]
         fit_array[ind] = fit_sum
     # dont give multiple cross with same parents
-    for k in range(int(round(len(popul)/2))):
+    for k in range(int(round(len(popul)/4))):
         # par1_ind = find_parent_index(popul,fit_array)
         # par2_ind = find_parent_index(popul,fit_array)
         par1_ind = k
@@ -268,8 +312,11 @@ def generate_next_population(popul):
         if(par1_ind==par2_ind):
             while(par1_ind==par2_ind):
                 par2_ind = find_parent_index(popul,fit_array)
+        
         par1 = popul[par1_ind]
         par2 = popul[par2_ind]
+        new_pop.append(par1)
+        new_pop.append(par2)
         new_ch1,new_ch2 = crossover(par1,par2)
         new_pop.append(new_ch1)
         new_pop.append(new_ch2)
@@ -292,7 +339,7 @@ def create_individual():
     while k<num_of_cubes:
         cubes[cub_list[k]] = np.random.randint(axis_size//2, size=3) # xmin, ymin, zmin
         # check if we found place where new cube is not overlaping other ones
-        if(not check_overlap(cubes,cub_list[k])): 
+        if(not current_cube_position_overlap(cubes,cub_list[k])): 
             k += 1
         
     return cubes
